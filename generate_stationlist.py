c3e0877e42ab004fdc5645bb988b42f7f2c92268
Takeshi KOMIYA <i.tkomiya@gmail.com>
2016-03-05 19:31:33 +0900

    Support geocoder

--- generate_stationlist.py
+++ generate_stationlist.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
 import io
 import os
+import sys
 import lxml.html
+import geocoder
 from time import sleep
 
 BASEURI = 'http://www.michi-no-eki.jp/'
@@ -16,20 +20,15 @@ def get_url(path):
 
 def get_prefectures():
     root = lxml.html.parse(get_url('/')).getroot()
-
-    prefs = []
     for pref in root.xpath('//div[@id="prefecture"]/div/div/a'):
-        prefs.append(dict(id=pref.get('id'),
-                          name=pref.text,
-                          uri=pref.get('href')))
-
-    return prefs
+        yield dict(pref_id=pref.get('id'),
+                   name=pref.text,
+                   uri=pref.get('href'))
 
 
 def get_stations(pref):
     root = lxml.html.parse(get_url(pref.get('uri'))).getroot()
 
-    stations = []
     for station in root.xpath('//ul[@id="searchList"]/li'):
         url = station.xpath('div[@class="name"]/a')[0].get('href')
         if url.endswith('/'):
@@ -37,36 +36,50 @@ def get_stations(pref):
         else:
             station_id = os.path.basename(url)
 
-        stations.append(dict(pref_id=pref.get('id'),
-                             pref_name=pref.get('name'),
-                             station_id=station_id,
-                             name=station.findtext('div[@class="name"]/a'),
-                             address=station.findtext('div[@class="address"]'),
-                             tel=station.findtext('div[@class="tel"]'),
-                             hours=station.findtext('div[@class="hours"]')))
+        address = station.findtext('div[@class="address"]').replace('\n', '')
+        try:
+            lat, lng = geocoder.google(address).latlng
+        except:
+            lat, lng = None, None
 
-    return stations
+        yield dict(pref_id=pref.get('pref_id'),
+                   pref_name=pref.get('name'),
+                   station_id=station_id,
+                   name=station.findtext('div[@class="name"]/a'),
+                   address=station.findtext('div[@class="address"]'),
+                   tel=station.findtext('div[@class="tel"]'),
+                   hours=station.findtext('div[@class="hours"]'),
+                   lat=lat,
+                   lng=lng)
 
 
-def get_all_stations():
-    for pref in get_prefectures():
-        sleep(FETCH_INTERVAL)
-        for station in get_stations(pref):
-            yield station
+def _print(text, flush=False, **kwargs):
+    print(text, **kwargs)
+    if flush:
+        sys.stdout.flush()
 
 
 def main():
     last_pref = None
     with io.open('data/stations.csv', 'w', encoding='utf-8') as f:
-        print 'Fetch list of prefectures...'
-        for station in get_all_stations():
-            if last_pref != station['pref_id']:
-                print 'Processing %s...' % station['pref_id']
-                last_pref = station['pref_id']
+        _print('Fetch list of prefectures ...', end='', flush=True)
+        prefs = list(get_prefectures())
+        _print(' done')
 
-            row = [station['pref_id'], station['station_id'],
-                   station['name'], station['address']]
-            f.write('\t'.join(row) + '\n')
+        for pref in prefs:
+            _print('Processing %s ...' % pref['pref_id'], end='', flush=True)
+            for station in get_stations(pref):
+                if last_pref != station['pref_id']:
+                    last_pref = station['pref_id']
+
+                row = [station['pref_id'], station['station_id'],
+                       station['name'], station['address'],
+                       str(station['lat']), str(station['lng'])]
+                f.write('\t'.join(row) + '\n')
+                _print('.', end='', flush=True)
+
+            _print(' done')
+            sleep(FETCH_INTERVAL)
 
 
 main()
