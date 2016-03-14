9069152af7000f0e75cc2e547c9627ce6a2e0dbf
Takeshi KOMIYA <i.tkomiya@gmail.com>
2016-03-14 12:49:57 +0900

    ES6ize RoadStation class

--- html/js/roadstation.js
+++ html/js/roadstation.js
@@ -1,24 +1,22 @@
-var RoadStation = (function() {
-    var STYLES = {
-        0: {icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'},
-        1: {icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'},
-        2: {icon: 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png'},
-        3: {icon: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'},
-        4: {icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'},
-    };
+var STYLES = {
+    0: {icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'},
+    1: {icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'},
+    2: {icon: 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png'},
+    3: {icon: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'},
+    4: {icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'},
+};
 
-    var station = function(feature) {
+class RoadStation {
+    constructor(feature) {
         this.feature = feature;
         this.pref_id = feature.getProperty("pref_id");
         this.station_id = feature.getProperty("station_id");
         this.name = feature.getProperty("name");
         this.address = feature.getProperty("address");
         this.style_id = this.getStyleId();
-    };
+    }
 
-    var proto = station.prototype;
-
-    proto.getStyleId = function() {
+    getStyleId() {
         var style_id = localStorage.getItem(this.station_id);
         if (style_id) {
             return parseInt(style_id);
@@ -34,17 +32,17 @@ var RoadStation = (function() {
         } else {
             return 0;
         }
-    };
+    }
 
-    proto.isVisited = function() {
+    isVisited() {
         return this.style_id;
-    };
+    }
 
-    proto.getStyle = function() {
+    getStyle() {
         return STYLES[this.style_id];
-    };
+    }
 
-    proto.changeStyle = function() {
+    changeStyle() {
         if (this.style_id >= 4) {
             this.resetStyle();
         } else {
@@ -54,13 +52,11 @@ var RoadStation = (function() {
         return this.getStyle();
     }
 
-    proto.resetStyle = function() {
+    resetStyle() {
         this.style_id = 0;
         localStorage.removeItem(this.station_id);
         return this.getStyle();
-    };
-
-    return station;
-})();
+    }
+}
 
 module.exports = RoadStation;
