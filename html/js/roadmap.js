d60b254f6e8e6dd9a9f6609e5459b4b1d72dccd2
Takeshi KOMIYA <i.tkomiya@gmail.com>
2016-03-15 11:24:19 +0900

    React-ize infowindow

--- html/js/roadmap.js
+++ html/js/roadmap.js
@@ -5,6 +5,69 @@ var request = require('request');
 
 var RoadStation = require('./roadstation');
 
+var InfoWindow = React.createClass({
+    propTypes: {
+        element: React.PropTypes.object,
+        map: React.PropTypes.object,
+        onClick: React.PropTypes.func
+    },
+    getInitialState: function() {
+        return { feature: null };
+    },
+    componentDidMount: function() {
+        this.infowindow = new google.maps.InfoWindow();
+        this.infowindow.addListener("closeclick", this.close);
+        this.infowindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
+    },
+    componentDidUpdate: function() {
+        this.infowindow.setContent(this.props.element);
+        if (this.state.feature) {
+            this.infowindow.open(this.props.map);
+        } else {
+            this.infowindow.close();
+        }
+    },
+    onClick: function() {
+        this.props.onClick(this.state.feature);
+    },
+    open: function(feature) {
+        this.setState({ feature: feature });
+    },
+    close: function() {
+        this.setState({ feature: null });
+    },
+    isOpenedFor: function(feature) {
+        if (this.infowindow.getMap() && this.state.feature == feature) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    render: function() {
+        if (this.state.feature == null) {
+            return <div />;
+        } else {
+            this.infowindow.setPosition(this.state.feature.getGeometry().get());
+            var station = new RoadStation(this.state.feature);
+            return (
+                <div>
+                    <div>{station.name}</div>
+                    <div>({station.address})</div>
+                    <a href="#" onClick={this.onClick}>マーカーの色を変える</a>
+                </div>
+            );
+        }
+    }
+});
+
+var InfoWindowFactory = function(map, onClick) {
+    var element = document.createElement("div");
+    return ReactDOM.render(
+        <InfoWindow map={map} onClick={onClick} element={element} />,
+        element
+    );
+};
+
 var RoadStationMap = React.createClass({
     getInitialState: function() {
         return { data: null };
@@ -14,31 +77,12 @@ var RoadStationMap = React.createClass({
             center: { lat: 35.6896342, lng: 139.6921007 }, // Shinjuku, Tokyo
             zoom: 9
         });
-        this.infowindow = new google.maps.InfoWindow();
-        this.initInfoWindow()
+        this.infowindow = InfoWindowFactory(this.map, this.onMarkerStyleModifierClicked);
 
         jQuery.getJSON('../data/stations.geojson', this.onGeoJSONLoaded);
     },
     render: function() {
         return React.createElement('div', { className: 'map-canvas' });
-    },
-    initInfoWindow: function() {
-        var self = this;
-        var switcher = jQuery('<a />').attr('href', '#')
-        switcher[0].innerText = 'マーカーの色を変える';
-        switcher.on('click', function() {
-            var station = new RoadStation(self.infowindow.feature);
-            self.map.data.overrideStyle(self.infowindow.feature, station.changeStyle());
-            self.infowindow.close();
-        });
-
-        var root = document.createElement("div");
-        jQuery('<div/>').appendTo(root);
-        jQuery('<div/>').appendTo(root);
-        jQuery('<div/>').append(switcher).appendTo(root);
-
-        this.infowindow.setContent(root);
-        this.infowindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
     },
     onGeoJSONLoaded: function(data) {
         this.map.addListener("click", this.onMapClicked);
@@ -52,16 +96,17 @@ var RoadStationMap = React.createClass({
     onMapClicked: function() {
         this.infowindow.close();
     },
+    onMarkerStyleModifierClicked: function(feature) {
+        var station = new RoadStation(feature);
+        this.map.data.overrideStyle(feature, station.changeStyle());
+        this.infowindow.close();
+    },
     onMarkerClicked: function(event) {
-        var station = new RoadStation(event.feature);
-        if (this.infowindow.getMap() && this.infowindow.feature == station.feature) {
+        if (this.infowindow.isOpenedFor(event.feature)) {
+            var station = new RoadStation(event.feature);
             this.map.data.overrideStyle(event.feature, station.changeStyle());
         } else {
-            this.infowindow.content.children[0].innerText = station.name;
-            this.infowindow.content.children[1].innerText = "(" + station.address + ")";
-            this.infowindow.setPosition(station.feature.getGeometry().get());
-            this.infowindow.feature = station.feature;
-            this.infowindow.open(this.map);
+            this.infowindow.open(event.feature)
         }
     },
     onMarkerDoubleClicked: function(event) {
