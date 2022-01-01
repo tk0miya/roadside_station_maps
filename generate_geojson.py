2af51ccaf0a293e35367f8eabc7619ad944c21f9
Takeshi KOMIYA <i.tkomiya@gmail.com>
2022-01-01 12:44:47 +0900

    Remove ID-mapping feature

--- generate_geojson.py
+++ generate_geojson.py
@@ -7,33 +7,20 @@ from geojson import Feature, Point, FeatureCollection
 from generate_stationlist import STATION_FILENAME, load_station_list
 
 
-STATION_ID_MAPPING_FILENAME = 'data/stations_id_mapping.csv'
-
-
-def load_station_id_mapping(filename):
-    mapping = {}
-    for station in load_station_list(filename):
-        mapping[station.name] = station.station_id
-
-    return mapping
-
-
-def convert_feature(station, old_station_ids):
+def convert_feature(station):
     if station.lat == 'None' or station.lng == 'None':
         return None
 
     geometry = Point((float(station.lng), float(station.lat)))
     properties = dict(pref_id=station.pref_id, station_id=station.station_id,
                       name=station.name, address=station.address,
-                      hours=station.hours, uri=station.uri,
-                      old_station_id=old_station_ids.get(station.name, None))
+                      hours=station.hours, uri=station.uri)
     return Feature(geometry=geometry, properties=properties)
 
 
 def main():
     stations = load_station_list(STATION_FILENAME)
-    old_station_ids = load_station_id_mapping(STATION_ID_MAPPING_FILENAME)
-    features = (convert_feature(s, old_station_ids) for s in stations)
+    features = (convert_feature(s) for s in stations)
     with io.open('data/stations.geojson', 'w', encoding='utf-8') as f:
         json = geojson.dumps(FeatureCollection(list(filter(bool, features))),
                              ensure_ascii=False)
